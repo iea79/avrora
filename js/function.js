81f199cb830e62ce4a4953ca130c9609ff9f3ec9
Evgeny Ivanov <busforward@gmail.com>
2016-07-03 22:51:14 +0500

    Add home page

--- js/function.js
+++ js/function.js
@@ -1,39 +1,28 @@
 $(document).ready(function() {
 
-	function setHeiHeight() {
-	    $('.full__height').css({
-	        minHeight: $(window).height() + 'px'
-	    });
-	}
-	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
-	$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
-
-
 	// Reset link whte attribute href="#"
 	$('[href*="#"]').click(function(event) {
 		event.preventDefault();
 	});
 
-	// Scroll to ID
-    $('#main__menu a[href^="#"]').click( function(){ 
-	    var scroll_el = $(this).attr('href'); 
-        if ($(scroll_el).length != 0) {
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-        }
-	    return false;
-    });
+	// Home slider
+	$('.main_slider').slick({
+		infinite: true,
+		dots: true,
+		arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 
-	// Stiky menu
-    $(document).ready(function(){
-        var HeaderTop = $('#header').offset().top;
-        
-        $(window).scroll(function(){
-                if( $(window).scrollTop() > HeaderTop ) {
-                        $('#header').addClass('stiky');
-                } else {
-                        $('#header').removeClass('stiky');
-                }
-        });
-    });
+	// Top menu hover
+	$('.navabr__link').on('mouseenter', function(event) {
+		event.preventDefault();
+		$('.navbar__drop').removeClass('open');
+		$(this).parent().find('.navbar__drop').addClass('open');
+	});
+	$('.navbar').on('mouseleave', function(event) {
+		event.preventDefault();
+		$('.navbar__drop').removeClass('open');
+	});
 
 });
